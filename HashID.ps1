34d2d7b0903625254b6899180b1b77b6648299e9
Rutendo-xx <rutendomazvi.rm@gmail.com>
2025-11-16 22:44:57 +0100

    Add files via upload

---
+++ HashID.ps1
@@ -0,0 +1,8 @@
+Install-Script -Name Get-WindowsAutoPilotInfo
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+New-Item -Type Directory -Path "C:\HWID"
+Set-Location -Path "C:\HWID"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutoPilotInfo
+Get-WindowsAutoPilotInfo -OutputFile AutopilotHWID.csv 
